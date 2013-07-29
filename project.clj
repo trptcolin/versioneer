8c9eb89927bd11662b1152f8f73024782eed67fb
Colin Jones <trptcolin@gmail.com>
2013-07-29 12:20:23 -0500

    Bump version to 0.1.1 for LICENSE

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trptcolin/versioneer "0.1.0"
+(defproject trptcolin/versioneer "0.1.1"
   :description "Provides version introspection for Leiningen-generated projects"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
