92884f8fadaca21ae43e416fb1c430a93d9b203c
Paul Stadig <paul@stadig.name>
2015-04-21 08:32:11 -0400

    Add test for get-revision.

--- test/trptcolin/versioneer/core_test.clj
+++ test/trptcolin/versioneer/core_test.clj
@@ -9,7 +9,7 @@
 
   (testing "with the system property unset and the properties file present"
     (System/clearProperty "bar.version")
-    ; from the actual file in test/META-INF/foo/bar/
+    ;; from the actual file in test/META-INF/foo/bar/
     (is (= "9.8.7" (get-version "foo" "bar"))))
 
   (testing "with both spots unset"
@@ -18,6 +18,21 @@
       (is (= "" (get-version "foo" "bar")))))
 
   (testing "with both spots unset and a default string passed"
+    (System/clearProperty "bar.version")
+    (with-redefs [map-from-property-filepath (fn [f] nil)]
+      (is (= "N/A" (get-version "foo" "bar" "N/A"))))))
+
+(deftest test-get-revision
+  (testing "with the properties file present"
+    ;; from the actual file in test/META-INF/foo/bar/
+    (is (= "0000000000000000000000000000000000000000\n"
+           (get-revision "foo" "bar"))))
+
+  (testing "with the properties file not present"
+    (with-redefs [map-from-property-filepath (fn [f] nil)]
+      (is (= "" (get-version "foo" "bar")))))
+
+  (testing "with the properties file not present and a default string passed"
     (System/clearProperty "bar.version")
     (with-redefs [map-from-property-filepath (fn [f] nil)]
       (is (= "N/A" (get-version "foo" "bar" "N/A"))))))
